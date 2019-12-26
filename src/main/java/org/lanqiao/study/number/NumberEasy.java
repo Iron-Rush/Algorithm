282fb4bb64e5111042e6c7d91f406b5bc302eef7
Iron-Rush <chenzilin97@gmail.com>
2019-12-26 22:47:57 +0800

    20191226

---
+++ src/main/java/org/lanqiao/study/number/NumberEasy.java
@@ -0,0 +1,41 @@
+package org.lanqiao.study.number;
+
+import org.junit.jupiter.api.Test;
+
+public class NumberEasy {
+    @Test
+    public void testSolution(){
+//        System.out.println(subtractProductAndSum(234));
+        int[] arr = new int[]{1,2,2,6,6,6,6,7,10};
+        System.out.println(findSpecialInteger(arr));
+    }
+
+    public int subtractProductAndSum(int n) {
+       int product = 1,sum = 0;
+       while (n >= 1){
+           int temp = n % 10;
+           sum += temp;
+           product *= temp;
+           System.out.println("product=" + product);
+           System.out.println("sum=" + sum);
+           n = n/10;
+       }
+       return  product - sum;
+    }
+
+    public int findSpecialInteger(int[] arr) {
+        int res = arr[0], len = arr.length;
+        int target = len / 4;
+        for (int i = 0; i < len; i++){
+            int count = 0, temp = arr[i];
+            while (i < len-1 && arr[i] == arr[i + 1]){
+                count++;
+                i++;
+            }
+            if (count > target){
+                return temp;
+            }
+        }
+        return res;
+    }
+}
