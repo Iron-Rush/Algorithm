5cf9e97566197e95fe2a0ebfecde165497473114
Black <1024169266@qq.com>
2020-09-19 16:03:06 +0800

    2020年9月19日 16:02:47，二叉树-404. 左叶子之和

---
+++ src/main/java/cn/czl/dynamicPlanning/backpack/Backpack01_Classic.java
@@ -0,0 +1,46 @@
+package cn.czl.dynamicPlanning.backpack;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author RedRush
+ * @date 2020/9/18 23:52
+ * @description:
+ *          0-1 背包问题
+ *          给你一个可装载重量为W的背包和N个物品，
+ *          每个物品有重量和价值两个属性。
+ *          其中第i个物品的重量为wt[i]，价值为val[i]，
+ *          现在让你用这个背包装物品，最多能装的价值是多少？
+ *      - 输入:
+ *          N = 3, W = 4
+ *          wt = [2, 1, 3]
+ *          val = [4, 2, 3]
+ *      - 输出: 6
+ */
+public class Backpack01_Classic {
+    private static int N = 3, W = 4;
+    private static int[] wt = new int[]{2,1,3};
+    private static int[] val = new int[]{4,2,3};
+
+
+    @Test
+    public void TestSolution(){
+        System.out.println(backpack01(4,3,wt,val));
+    }
+
+    public int backpack01(int weight, int n, int[] wt, int[] val){
+        int[][] dp = new int[n+1][weight+1];    // 从背包容量为0开始，向上层递推最优解
+        for (int i = 1; i <= n; i++) {          // 背包物品容量递推层
+            for (int w = 1; w <= weight; w++) { // 背包物品重量递推层
+                if (w - wt[i-1] < 0){   // 若当前背包塞不下，则不装入背包(则该位置为前一个背包物品容量的解)
+                    dp[i][w] = dp[i-1][w];
+                }else {                 // 择优，判断是否装入背包
+                    dp[i][w] = Math.max(dp[i-1][w-wt[i-1]] + val[i-1],
+                                        dp[i-1][w]);
+                }
+            }
+        }
+        return dp[n][weight];
+    }
+
+}
---
+++ src/main/java/cn/czl/node/treenode/SumOfLeftTree_Easy.java
@@ -0,0 +1,71 @@
+package cn.czl.node.treenode;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author RedRush
+ * @date 2020/9/19 15:10
+ * @description:
+ *      404. 左叶子之和
+ *          计算给定二叉树的所有左叶子之和。
+ *      示例：
+ *              3
+ *             / \
+ *            9  20
+ *              /  \
+ *             15   7
+ *      在这个二叉树中，有两个左叶子，分别是 9 和 15，所以返回 24
+ */
+public class SumOfLeftTree_Easy {
+
+    @Test
+    public void TestSolution(){
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        System.out.println(sumOfLeftLeaves(root));
+    }
+
+    /**
+     * dfs深度优先搜索
+     * 执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
+     * 内存消耗：36.6 MB, 在所有 Java 提交中击败了93.17%的用户
+     * */
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null){
+            return 0;
+        }
+        int sum = 0;
+        sum = dfs(root, sum, false);
+        return sum;
+    }
+    public int dfs(TreeNode curNode, int sum, boolean flag){
+        if (curNode.left != null){  // 将现有数字和，下传，进行累加
+            sum = dfs(curNode.left, sum, true);
+        }
+        if (curNode.right != null){
+            sum = dfs(curNode.right, sum, false);
+        }
+        if (flag && curNode.left == null && curNode.right == null){
+            sum += curNode.val;
+        }
+        return sum;
+    }
+
+    /**
+     * 极简-直接递归
+     * 执行用时：0 ms, 在所有 Java 提交中击败了100.00%的用户
+     * 内存消耗：37 MB, 在所有 Java 提交中击败了28.01%的用户
+     * */
+    public int sumOfLeftLeaves2(TreeNode root) {
+        if (root == null){
+        return 0;
+        }
+        return sumOfLeftLeaves2(root.left)
+                + sumOfLeftLeaves2(root.right)
+                + ((root.left != null && root.left.left == null && root.left.right == null) ? root.left.val : 0);
+    }
+
+}
