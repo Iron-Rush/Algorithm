cb8f065064ce58863a29360437fdb8ec802c0c4c
Black <1024169266@qq.com>
2020-11-12 18:10:05 +0800

    2020年11月12日 18:09:54，514. 自由之路

--- src/main/java/cn/czl/search/str/FindRotateSteps_Hard.java
+++ src/main/java/cn/czl/search/str/FindRotateSteps_Hard.java
@@ -32,7 +32,7 @@ public class FindRotateSteps_Hard {
 
     @Test
     public void TestSolution(){
-        System.out.println(findRotateSteps(RING1, KEY1));
+        System.out.println(findRotateSteps4(RING1, KEY1));
     }
 
     /**
@@ -77,26 +77,18 @@ public class FindRotateSteps_Hard {
 
     // 获取顺时针旋转的下一个指针
     int rotateRight(char[] dic, int pos){
-//        if (pos == dic.length-1){
-//            return 0;
-//        }else {
-//            return pos + 1;
-//        }
         return pos == dic.length-1 ? 0 : pos + 1;
     }
     // 获取逆时针旋转的下一个指针
     int rotateLeft(char[] dic, int pos){
-//        if (pos == 0){
-//            return dic.length-1;
-//        }else {
-//            return pos - 1;
-//        }
         return pos == 0 ? dic.length-1 : pos - 1;
     }
 
     /**
-     * 动态规划
+     * 执行用时：38 ms, 在所有 Java 提交中击败了9.59%的用户
+     * 内存消耗：39.2 MB, 在所有 Java 提交中击败了58.39%的用户
      * */
+    Map<String, Integer> memo = new HashMap<>();
     public int findRotateSteps2(String ring, String key) {
         Map<Character, List<Integer>> map = new HashMap<>();
         for (int i = 0; i < ring.length(); i++) {
@@ -106,28 +98,65 @@ public class FindRotateSteps_Hard {
             }
             map.get(k).add(i);
         }
-        int rLen = ring.length(), kLen = key.length();
+        return key.length() + dfs(ring, key, 0 ,0, map);
+    }
+    // 将罗盘存入Map，取值进行递归
+    int dfs(String ring, String key, int rPos, int kPos, Map<Character, List<Integer>> map){
+        if (kPos == key.length()){
+            return 0;
+        }
+        String mkey = rPos + "-" + kPos;
+        if (memo.containsKey(mkey)){
+            return memo.get(mkey);
+        }
+        char curKey = key.charAt(kPos);
+        int dist = Integer.MAX_VALUE;
+        for (int ringPos : map.get(curKey)){
+            int dist1 = Math.abs(rPos - ringPos);
+            int dist2 = ring.length() - dist1;
+            int curMin = Math.min(dist1, dist2);
+            dist = Math.min(dist, curMin + dfs(ring, key, ringPos, kPos + 1, map));
+        }
+        memo.put(mkey, dist);
+        return dist;
+    }
 
+    /**
+     * 动态规划
+     * */
+    public int findRotateSteps4(String ring, String key) {
+        // map主要用于保存ring里字符所有的index
+        Map<Character, List<Integer>> map = new HashMap<>();
+        // 初始化map,键:ring中的字符,值:ring对应的全部index
+        for (int i = 0; i < ring.length(); i++) {
+            char k= ring.charAt(i);
+            if (!map.containsKey(k)) {
+                map.put(k, new LinkedList<Integer>());
+            }
+            map.get(k).add(i);
+        }
+        int rLen = ring.length(), kLen = key.length();
         // dp[i][j] means that min cost from key[i] to ring[j]
         int[][] dp = new int[kLen][rLen];
         for (int i = 0; i < kLen; i++) {
             Arrays.fill(dp[i], Integer.MAX_VALUE);
         }
-        for (int j : map.get(key.charAt(0))) {
+        for (int j : map.get(key.charAt(0))) {      // 初始化dp数组第一行，第一位密码的答案
             dp[0][j] = Math.min(j, rLen - j);
         }
-        for (int i = 1; i < kLen; i++) {
-            for (int j : map.get(key.charAt(i))) {
-                int temp = Integer.MAX_VALUE;
-                for (int k : map.get(key.charAt(i - 1))) {
-                    // rotate the ring clockwise or anticlockwise
+        for (int i = 1; i < kLen; i++) {            // 遍历全部密码
+            for (int j : map.get(key.charAt(i))) {  // 遍历当前密码的全部答案
+                int temp = Integer.MAX_VALUE;       // 与之前密码答案进行匹配，累加
+                for (int k : map.get(key.charAt(i - 1))) {  // (获取前一位密码答案)
+                    // 当前距离的两种计算方式( abs(j-k) / rLen-abs(j-k) )，-> 择优
                     temp = Math.min(temp, dp[i - 1][k] + Math.abs(j - k));
                     temp = Math.min(temp, dp[i - 1][k] + rLen - Math.abs(j - k));
                 }
-                dp[i][j] = temp;
+                dp[i][j] = temp;    // 填充当前罗盘位置，最优答案
             }
         }
-        return Arrays.stream(dp[kLen - 1]).min().getAsInt() + kLen;
+        System.out.println(Arrays.deepToString(dp));
+        return Arrays.stream(dp[kLen - 1]).min().getAsInt() + kLen;// 获取最后一行答案中的最小值 + 密码长度
     }
 
 }
